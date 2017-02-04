1715aa95ca6299181f39f1a523df081b4fa663b1
m0t0r <tkachenko.vitaly.job@gmail.com>
2017-02-04 16:41:28 -0500

    feat(components): Create 'experiment' component

---
+++ src/app/sections/shared/components/experiment/experiment.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'experiment',
+  templateUrl: './experiment.component.html',
+  styleUrls: ['./experiment.component.scss']
+})
+export class ExperimentComponent { }
