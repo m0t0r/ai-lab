74e966834a08914077baf6b39e8d5cffe92307b8
m0t0r <tkachenko.vitaly.job@gmail.com>
2017-02-04 18:04:44 -0500

    feat(components): Create ai-console component

---
+++ src/app/sections/shared/components/ai-console/ai-console.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ai-console',
+  templateUrl: './ai-console.component.html',
+  styleUrls: ['./ai-console.component.scss']
+})
+export class AiConsoleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() { }
+
+}
